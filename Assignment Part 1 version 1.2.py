350d2d5bc82aaed780c47abbdb100d05d1605d54
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2024-05-06 22:17:06 +1000

    Improved performance by using different module

---
+++ Assignment Part 1 version 1.2.py
@@ -0,0 +1,39 @@
+from sense_emu import SenseHat
+import requests as req
+
+server = 'http://iotserver.com/state-log.php'
+s = SenseHat()
+in_x = in_y = in_z = 0
+state = 'normal'
+
+while True:
+    acc = s.get_accelerometer()
+    x = round(acc['pitch'],2)
+    y = round(acc['yaw'],2)
+    z = round(acc['roll'],2)
+    if (abs(x - in_x) > 0.3 or abs(y - in_y) > 0.3 or abs(z - in_z) > 0.3):
+        state = 'collision'
+        s.show_message("Collision")
+        #report collision to server
+        payload = {'state' : state}
+        sr = req.get(server, params = payload)
+        for event in s.stick.get_events():
+            if event.action in ('pressed'):
+                if event.direction in ('middle'):
+                    state = 'normal'
+                    s.clear()
+                    print(state)
+        
+    elif(abs(x - in_x) > 0.1 or abs(y - in_y) > 0.1 or abs(z - in_z) > 0.1):
+        state = 'windy'
+        # report to server here 
+        payload = {'state' : state}
+        sr = req.get(server, params = payload)
+        state = 'normal' # changing state to normal after reporting to the server
+        # print(sr.text)
+        print(state)
+    else:
+        state = 'normal'
+    in_x = x
+    in_y = y
+    in_z = z
