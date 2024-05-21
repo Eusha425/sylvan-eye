643b1cb47fa7eb997a9985e5748ce8ff43ae222c
Gazi Eusha <52084779+Eusha425@users.noreply.github.com>
2024-05-21 21:13:59 +1000

    Add latest iteration of the webserver code

---
+++ webserver.php
@@ -0,0 +1,30 @@
+<?php 
+	error_reporting(E_ALL);						//show any errors if there is any
+	ini_set('display_errors', '1');
+
+
+	if(isset($_GET['temperature']) && isset($_GET['humidity']) && isset($_GET['condition']) && isset($_GET['timestamp']) && isset($_GET['temp_thresh']) && isset($_GET['humidity_thresh'])) { 					// Check if temprature is present or not
+		
+		$str = '';								// intialize empty string
+
+
+		if(file_exists('sensorData.xml')) {				// if the xml file already exists then read it
+			$str = file_get_contents('sensorData.xml');	// get all the current data
+		}
+		
+		if(strlen($str) == 0) {
+			// intialize the variable to the empty xml if there is no old content
+			$str = "<?xml version='1.0' encoding='UTF-8'?> \n<records></records>";
+		}
+
+		// create a new text for appending to the file
+		$newData = "\n<record>\n<sensor>\n<temperature>". $_GET['temperature']. "</temperature>\n<humidity>". $_GET['humidity']."</humidity>\n"."<temperature_threshold>" . $_GET['temp_thresh'] . "</temperature_threshold>\n<humidity_threshold>". $_GET['humidity_thresh'] ."</humidity_threshold>\n<condition>". $_GET['condition'] ."</condition>\n". "<timestamp>". $_GET['timestamp'] ."</timestamp>\n</sensor>\n<date>". date('d-m-Y H:i:s') . "</date>\n</record>\n</records>"; 	
+		$str = str_replace("</records>", $newData, $str);	// put the data in the end of the xml document
+		
+		file_put_contents('sensorData.xml', $str);			// write the file back to the server
+
+		echo '1';							// return success
+	}
+	else
+		echo '0';							// return failure
+?>
